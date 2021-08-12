0e6c23224a36b686f786dafdbc2c332fa4a74719
Leonardo Reis <leonardo.demian@gmail.com>
2021-08-11 21:54:40 -0300

    framework express e início da API REST

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+const array1 = ['posição0', 'posição1', 'posição2'];
+
+//retornar um curso por INDEX (posição)
+server.get('/teste/:index', (req, res)) => {
+    const { index } = req.params;
+
+    return res.json(array1[index]);
+}
+
+
+
+
+
+
+server.listen(3000);
