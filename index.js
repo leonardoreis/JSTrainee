15284a2da01bb4ebd77c36b27f342fe06aa23d20
Leonardo Reis <leonardo.demian@gmail.com>
2021-08-11 22:10:28 -0300

    alteraçao no index.js

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ server.use(express.json());
 const array1 = ['posição0', 'posição1', 'posição2'];
 
 //retornar um curso por INDEX (posição)
-server.get('/teste/:index', (req, res)) => {
+server.get('/teste/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(array1[index]);
-}
+});
 
-
+server.get('/',(req, res)=>{
+    return res.json(array1);
+});
 
 
 
