0f09bf5f8d6b2160b251463914cf9cc420f758a3
Leonardo Reis <leonardo.demian@gmail.com>
2021-08-17 22:35:05 -0300

    teste

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const server = express();
 server.use(express.json());
 const array1 = ['posição0', 'posição1', 'posição2'];
 
+var pessoa = {
+    nome: ['Bob', 'Smith'],
+    idade: 32,
+    sexo: 'masculino',
+    interesses: ['música', 'esquiar'],
+  };
+
+
 //retornar um curso por INDEX (posição)
 server.get('/teste/:index', (req, res) => {
     const { index } = req.params;
@@ -14,6 +22,10 @@ server.get('/teste/:index', (req, res) => {
 
 server.get('/',(req, res)=>{
     return res.json(array1);
+});
+
+server.get('/json/',(req, res)=>{
+    return res.json(pessoa);
 });
 
 
